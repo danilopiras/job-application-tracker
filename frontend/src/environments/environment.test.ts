0f1135aaa4c5e88c2b187ef4b3b0964dd6eb23e7
Danilo <danilo.piras-cons@nord-com.it>
2026-04-08 17:23:39 +0200

    Profiling

---
+++ frontend/src/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiBaseUrl: 'http://localhost:9000',
+  defaultLang: 'en',
+  availableLangs: ['en', 'it', 'es', 'fr', 'pt', 'de'] as const,
+};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,1 @@
-export const environment = {
-  apiBaseUrl: 'http://localhost:9000',
-  defaultLang: 'en',
-  availableLangs: ['en', 'it', 'es', 'fr', 'pt', 'de'] as const,
-};
-
+export { environment } from './environment.test';
